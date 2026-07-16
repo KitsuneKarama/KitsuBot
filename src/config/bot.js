049a2996f81791b27a8981005d1e5fc8d56506e5
KitsuneKarama <43977942+KitsuneKarama@users.noreply.github.com>
2026-07-16 01:27:32 -0700

    Change activity type from 1 to 0 in bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
       {
         name: "kitsune break my code", // required by Discord API, not shown in the client
         state: "KitsuneKarama is stinky",     // this is what people actually see
-        type: 1,               // Custom
+        type: 0,               // Custom
       },
     ],
   },
